50d7dbfffa1c76ff5aeed7bef1a6d638da2eb2c5
balaramakrishna-kamma <balaramakrishna.kamma@bilvantis.io>
2025-06-02 14:47:55 +0530

    changes teh response tab when sql file is processed

--- shared/constants/index.ts
+++ shared/constants/index.ts
@@ -1,4 +1,5 @@
-export const APP_NAME = "SQL Analyzer Pro";
+export const COMPANY_NAME = "Bilvantis Technologies";
+export const APP_TITLE = "SQL Analyzer Pro";
 
 export const BLOCK_TYPES = [
   { value: "Stored Procedure", label: "Stored Procedure" },
